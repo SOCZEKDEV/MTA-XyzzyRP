7bb2893747f3d99c20b16720c4e791adb1c00967
SOCZEKDEV <103373053+SOCZEKDEV@users.noreply.github.com>
2025-05-04 05:20:36 +0200

    Update sql.lua
    Zastąpiono przestarzały moduł mta_mysql natywnym systemem dbConnect/dbQuery

--- resources/[XyzzyRP]/DB/sql.lua
+++ resources/[XyzzyRP]/DB/sql.lua
@@ -1,102 +1,62 @@
 --[[
-Obsluga baz danych, interfejs do bazy MySQL realizowany za pomoca modułu mta_mysql
+Obsluga baz danych, interfejs do bazy MySQL realizowany za pomoca natywnego systemu dbConnect
 
 @author Lukasz Biegaj <wielebny@bestplay.pl>
 @copyright 2011-2013 Lukasz Biegaj <wielebny@bestplay.pl>
 @license Dual GPLv2/MIT
 ]]--
 
-local SQL_LOGIN="z"
-local SQL_PASSWD="x"
-local SQL_DB="y"
-local SQL_HOST="tiger.og-servers.net"
-local SQL_PORT=tonumber(get("port") or 3306)
-
-local root = getRootElement()
+local SQL_LOGIN = "z"
+local SQL_PASSWD = "x"
+local SQL_DB = "y"
+local SQL_HOST = "tiger.og-servers.net"
+local SQL_PORT = 3306
 
 local SQL
 
 local function connect()
-	SQL = mysql_connect(SQL_HOST, SQL_LOGIN, SQL_PASSWD, SQL_DB, SQL_PORT)
-	if (not SQL) then
-		outputServerLog("BRAK POLACZENIA Z BAZA DANYCH!")
-	else
-		mysql_query(SQL,"SET NAMES utf8")
-	end
-
+    SQL = dbConnect("mysql", string.format("dbname=%s;host=%s;port=%d", SQL_DB, SQL_HOST, SQL_PORT), SQL_LOGIN, SQL_PASSWD, "share=1")
 end
-
 
 local function keepAlive()
-	if (not mysql_ping(SQL)) then
-		outputServerLog("Zerwane polaczenie z baza danych, nawiazywanie...")
-		connect()
-	end
+    if not SQL then
+        connect()
+    end
 end
 
-addEventHandler("onResourceStart",getResourceRootElement(),function()
-	connect()
-	setTimer(keepAlive, 30000, 0)
+addEventHandler("onResourceStart", resourceRoot, function()
+    connect()
+    setTimer(keepAlive, 30000, 0)
 end)
 
-function esc(value)
-	return mysql_escape_string(SQL,value)
+function queryFetchAll(query, ...)
+    local qh = dbQuery(SQL, query, ...)
+    local result = dbPoll(qh, -1)
+    return result
 end
 
-function pobierzTabeleWynikow(query)
-	local result=mysql_query(SQL,query)
-	if (not result) then 
-		outputDebugString("mysql_query failed: (" .. mysql_errno(SQL) .. ") " .. mysql_error(SQL)) -- Show the reason
-		return nil 
-	end
-	local tabela={}
-	for result,row in mysql_rows_assoc(result) do
-		table.insert(tabela,row)
-	end
-	mysql_free_result(result)
-	return tabela
+function queryFetchOne(query, ...)
+    local qh = dbQuery(SQL, query, ...)
+    local result = dbPoll(qh, -1)
+    return result and result[1] or nil
 end
 
-function pobierzWyniki(query)
-	local result=mysql_query(SQL,query)
-	if (not result) then return nil end
-	row = mysql_fetch_assoc(result)
-	mysql_free_result(result)
-	return row
-end
-
-
-function zapytanie(query)
-	local result=mysql_query(SQL,query)
-	if (result) then mysql_free_result(result) end
-	return
+function executeQuery(query, ...)
+    dbExec(SQL, query, ...)
 end
 
 function insertID()
-	return mysql_insert_id(SQL)
+    local qh = dbQuery(SQL, "SELECT LAST_INSERT_ID() AS id")
+    local result = dbPoll(qh, -1)
+    return result and result[1] and tonumber(result[1].id) or nil
 end
 
 function affectedRows()
-	return mysql_affected_rows(SQL)
+    local qh = dbQuery(SQL, "SELECT ROW_COUNT() AS count")
+    local result = dbPoll(qh, -1)
+    return result and result[1] and tonumber(result[1].count) or nil
 end
-
-
-function fetchRows(query)
-	local result=mysql_query(SQL,query)
-	if (not result) then return nil end
-	local tabela={}
-
-	while true do
-    	local row = mysql_fetch_row(result)
-	    if (not row) then break end
-	    table.insert(tabela,row)
-	end
-	mysql_free_result(result)
-	return tabela
-end
-
 
 function getSQLLink()
-	return SQL
+    return SQL
 end
-
